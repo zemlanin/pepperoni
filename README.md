157b26e217914790b454a69e715940e7f0f6ea7c
Anton Verinov <git@zemlan.in>
2020-03-07 19:22:11 +0200

    retries, whole-body-diff, verbosity

---
+++ README.md
@@ -0,0 +1,7 @@
+# pepperoni
+
+Query URL for content
+
+`python3 -m pepperoni -h`
+
+`python3 -m doctest pepperoni.py`
--- pepperoni.py
+++ pepperoni.py
@@ -1,36 +1,94 @@
 #!/usr/bin/env python3
-import re
+
+# spices
 import os
-import argparse
-import logging
+import time
+
+# sause
+import re
+import difflib
+import textwrap
 import html.parser
+
+# meat
 import urllib.request
 
-logger = logging.getLogger("pepperoni")
-logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+# dough
+import logging
+import argparse
 
 parser = argparse.ArgumentParser(
     description="Query URL for content",
     formatter_class=argparse.RawDescriptionHelpFormatter,
-    epilog="""examples:
-    > %(prog)s "https://docs.python.org"
-    > %(prog)s "https://docs.python.org" -s ".document h1"
-    > %(prog)s "https://docs.python.org" -r "Python ([0-9.]+)\"""",
+    epilog=textwrap.dedent(
+        """
+        examples:
+          > %(prog)s "https://docs.python.org" -q ".document h1"
+          > %(prog)s "https://docs.python.org" -r "Python ([0-9.]+)"
+          > %(prog)s "https://time.is" -w -u -i 5
+          > %(prog)s "https://time.is" -q time -i 5
+        """
+    ),
 )
 parser.add_argument("url", help="an url to request")
 parser.add_argument(
+    "--whole",
+    "-w",
+    default=False,
+    action="store_true",
+    help="match whole response body (ignore -q and -r arguments)",
+)
+parser.add_argument(
+    "--query",
     "-q",
-    dest="query",
     metavar="SELECTOR",
     type=str,
     help="a CSS-like selector to query (supports `tag`, `.class` and `#id`)",
 )
 parser.add_argument(
-    "-r", dest="regex", metavar="REGEX", type=re.compile, help="a regular expression",
+    "--regex", "-r", metavar="REGEX", type=re.compile, help="a regular expression",
 )
+parser.add_argument(
+    "--interval", "-i", metavar="SECONDS", type=int, help="interval between queries",
+)
+parser.add_argument(
+    "--until-change",
+    "-u",
+    default=False,
+    action="store_true",
+    help="retry until match is changed",
+)
+parser.add_argument("--verbose", "-v", action="count", default=0, help="verbose output")
 
 
 def get_selector(s):
+    """
+    >>> tag_selector = get_selector("h1")
+    >>> tag_selector("div", ())
+    False
+    >>> tag_selector("h1", ())
+    True
+
+    >>> id_selector = get_selector("#uniq")
+    >>> id_selector("div", (("class", "cls"), ))
+    False
+    >>> id_selector("div", (("id", ""), ))
+    False
+    >>> id_selector("div", (("id", "uniq"), ))
+    True
+
+    >>> class_selector = get_selector(".cls")
+    >>> class_selector("div", (("id", "uniq"), ))
+    False
+    >>> class_selector("div", (("class", ""), ))
+    False
+    >>> class_selector("div", (("class", "xxcls"), ))
+    False
+    >>> class_selector("div", (("class", "cls"), ))
+    True
+    >>> class_selector("div", (("class", "cls another"), ))
+    True
+    """
     if s.startswith("#"):
         return lambda tag, attrs: any(
             name == "id" and s[1:] == value for (name, value) in attrs
@@ -88,31 +146,111 @@ class HTMLParser(html.parser.HTMLParser):
                 self.match = data
 
 
-def main():
-    args = parser.parse_args()
-    logger.debug(args)
+def query_html(html, query, regex):
+    """
+    >>> html = "<ul><li>A</li><li id='b'>B</li></ul><ol><li>C</li><li class='d'>D</li></ol>"
+    >>> query_html(html, "li", None)
+    'A'
+    >>> query_html(html, "#b", None)
+    'B'
+    >>> query_html(html, "ol li", None)
+    'C'
+    >>> query_html(html, ".d", None)
+    'D'
+    >>> query_html(html, "ul li", "B|X")
+    'B'
+    >>> query_html(html, "ol li", "B|X")
+    """
+    if query:
+        html_parser = HTMLParser(query, regex)
+        html_parser.feed(html)
+        return html_parser.match
+    elif regex:
+        match = re.search(regex, html)
+        if match:
+            return match.group(0)
 
-    response = urllib.request.urlopen(args.url)
+    return None
+
+
+def request_and_query(url, whole, query, regex):
+    response = urllib.request.urlopen(
+        urllib.request.Request(
+            url, headers={"Accept": "text/html", "User-Agent": "Mozilla/5.0"}
+        )
+    )
 
     if response.status != 200:
-        print(response.status, response.reason)
-        return
+        logging.warning(response.status, response.reason)
+        return None
 
-    data1 = response.read().decode("utf-8")
+    response_body = response.read().decode("utf-8")
 
-    result = ""
+    if whole:
+        return response_body
 
-    if args.query:
-        html_parser = HTMLParser(query=args.query, regex=args.regex)
-        html_parser.feed(data1)
-        result = html_parser.match
-    elif args.regex:
-        match = re.search(args.regex, data1)
-        if match:
-            result = match.group(0)
+    return query_html(response_body, query, regex)
 
-    print(result or "no matches")
+
+def pepperoni(url, whole, query, regex, interval, until_change):
+    result = request_and_query(url, whole, query, regex)
+
+    if result is None:
+        logging.warning("no matches")
+    elif until_change and whole:
+        logging.info("%s bytes", len(result))
+    else:
+        logging.info(result)
+
+    while interval is not None:
+        logging.debug("doing to sleep for %s seconds", interval)
+        time.sleep(interval)
+
+        prev = result
+        result = request_and_query(url, whole, query, regex)
+
+        if result is None:
+            logging.warning("no matches")
+        elif until_change and whole:
+            logging.info("%s bytes", len(result))
+        else:
+            logging.info(result)
+
+        if prev != result:
+            print("\a", end="", flush=True)
+
+            if whole and prev and result:
+                d = difflib.Differ()
+
+                logging.info(
+                    "\n".join(
+                        difflib.unified_diff(
+                            textwrap.wrap(prev), textwrap.wrap(result), lineterm="", n=1
+                        )
+                    )
+                )
+
+            if until_change:
+                break
 
 
 if __name__ == "__main__":
-    main()
+    args = parser.parse_args()
+
+    logFormat = ""
+    if args.verbose:
+        logFormat = "%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s"
+
+    logLevel = logging.INFO
+    if args.verbose >= 2:
+        logLevel = logging.DEBUG
+
+    logging.basicConfig(
+        format=logFormat, datefmt="%Y-%m-%dT%H:%M:%S", level=logLevel,
+    )
+
+    logging.debug(args)
+
+    pepperoni(
+        args.url, args.whole, args.query, args.regex, args.interval, args.until_change
+    )
